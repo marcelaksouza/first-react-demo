c57368d8e7c4462714a952956b7a5439cc91dfb5
marcelaksouza <ma.karoline@hotmail.com>
2021-05-24 10:29:48 +0100

    form style

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,7 +3,9 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = (props) => {
+  //transform the object data point into an array of those values
   const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  //across all data points values return the max
   const totalMaximum = Math.max(...dataPointValues);
 
   return (
